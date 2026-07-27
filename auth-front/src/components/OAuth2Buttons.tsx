7fd94da58d8192b2962984fc388089eaab63fc51
vivek kumavat <kumavatvivek61@gmail.com>
2026-07-27 19:09:59 +0530

    Fix OAuth URLs

--- auth-front/src/components/OAuth2Buttons.tsx
+++ auth-front/src/components/OAuth2Buttons.tsx
@@ -1,40 +1,41 @@
 import React from "react";
 import { Button } from "./ui/button";
 import { Chrome, Github } from "lucide-react";
-import { NavLink } from "react-router";
+
+const BACKEND_URL =
+  import.meta.env.VITE_BACKEND_URL ||
+  "https://auth-app-boot-react.onrender.com";
 
 function OAuth2Buttons() {
   return (
     <div className="space-y-3">
-      <NavLink
-        to={`${
-          import.meta.env.VITE_BASE_URL || "http://localhost:8083"
-        }/oauth2/authorization/google`}
-        className={"block"}
+      <a
+        href={`${BACKEND_URL}/oauth2/authorization/google`}
+        className="block"
       >
         <Button
           type="button"
           variant="outline"
           className="w-full cursor-pointer flex items-center gap-3 rounded-2xl"
         >
-          <Chrome className="w-5 h-5" /> Continue with Google
+          <Chrome className="w-5 h-5" />
+          Continue with Google
         </Button>
-      </NavLink>
+      </a>
 
-      <NavLink
-        to={`${
-          import.meta.env.VITE_BASE_URL || "http://localhost:8083"
-        }/oauth2/authorization/github`}
-        className={"block"}
+      <a
+        href={`${BACKEND_URL}/oauth2/authorization/github`}
+        className="block"
       >
         <Button
           type="button"
           variant="outline"
-          className="w-full flex cursor-pointer items-center gap-3 rounded-2xl"
+          className="w-full cursor-pointer flex items-center gap-3 rounded-2xl"
         >
-          <Github className="w-5 h-5" /> Continue with GitHub
+          <Github className="w-5 h-5" />
+          Continue with GitHub
         </Button>
-      </NavLink>
+      </a>
     </div>
   );
 }
